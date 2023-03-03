a17254f2381f42b1b39a8920280bc8dff5e7852e
pdmduc <phandinhminhduc@gmail.com>
2023-03-03 22:49:53 +1100

    Draw SVG Logo Animation

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import LogoTitle from '../../assets/images/logo-s.png'
 import AnimatedLetters from '../AnimatedLetters'
+import Logo from './Logo'
 import './index.scss'
 
 const Home = () => {
@@ -62,6 +63,7 @@ const Home = () => {
           CONTACT ME
         </Link>
       </div>
+      <Logo />
     </div>
   )
 }

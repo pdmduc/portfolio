9c4cfceae400644daf925b7aadf67a375fd7bf56
pdmduc <phandinhminhduc@gmail.com>
2023-03-06 23:24:47 +1100

    Build map location on contact page
    Integrate react-leafletto build an interactive map

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
 import emailjs from '@emailjs/browser'
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
 
 const Contact = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -82,6 +83,25 @@ const Contact = () => {
               </ul>
             </form>
           </div>
+        </div>
+        <div className="info-map">
+          Dinh Phan (Max),
+          <br />
+          Sydney,
+          <br />
+          NSW, 2000
+          <br />
+          Australia
+          <br />
+          <span>phandinhminhduc@gmail.com</span>
+        </div>
+        <div className="map-wrap">
+          <MapContainer center={[-33.8837487, 151.1994824]} zoom={13}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={[-33.8837487, 151.1994824]}>
+              <Popup>Dinh lives here, come over for a cup of coffee :D</Popup>
+            </Marker>
+          </MapContainer>
         </div>
       </div>
     </>
